799f1d10d4b49cfa360de6be0ca6efdfbc137b4a
Siraj Ali <siraj@example.com>
2025-09-09 19:30:53 +0000

    Regenerated vendor after rebase

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -14,9 +14,12 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    throw new RuntimeException($err);
+    trigger_error(
+        $err,
+        E_USER_ERROR
+    );
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2185d2f99bcd56787481d9357a5972d3::getLoader();
+return ComposerAutoloaderInit8a0672f3f87447cccdef87e5ae7940a8::getLoader();
