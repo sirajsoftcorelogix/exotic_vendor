0a7cff2b1ebd0bc8dfe224a43d6a804219c2d322
Hedayat-dev <hedayat9831031564@outlook.com>
2025-10-03 22:12:32 +0530

    invoice details required validation

--- controllers/PurchaseOrdersController.php
+++ controllers/PurchaseOrdersController.php
@@ -304,6 +304,10 @@ class PurchaseOrdersController {
             exit;
            
         }
+        if(!$invoiceId){
+            echo json_encode(['success' => false, 'message' => 'Invoice ID is required.']);
+            exit;
+        }
         // Add payment to the invoice
         $paymentData = [
             'id' => isset($_POST['id']) ? $_POST['id'] : 0,
