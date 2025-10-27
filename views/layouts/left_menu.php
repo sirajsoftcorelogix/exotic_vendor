05157058c9581f986b78909f4b3a64661dd2f8ac
Swatantra Bhargava <swatantrab@midaswebtech.com>
2025-10-27 18:52:03 +0530

    Correct menu link, change 'orderstatus' to 'orders_priority_status'

--- views/layouts/left_menu.php
+++ views/layouts/left_menu.php
@@ -504,7 +504,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="<?=base_url('index.php?page=orders_priority_status&action=list');?>" class="nav-link text-gray-800 <?= ($page == 'orderstatus') ? 'active' : '' ?>">
+                        <a href="<?=base_url('index.php?page=orders_priority_status&action=list');?>" class="nav-link text-gray-800 <?= ($page == 'orders_priority_status') ? 'active' : '' ?>">
                             <div class="content-wrapper">
                               <i class="fa fa-clipboard-list mr-2"></i>
                               <span>Order Priority Status</span>
