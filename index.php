f248d10b0857d4e75a49a0b9178f9a4cd46f7d07
Hedayat-dev <hedayat9831031564@outlook.com>
2025-11-20 18:39:04 +0530

    stock purchase

--- index.php
+++ index.php
@@ -236,6 +236,9 @@ switch ($page) {
             case 'product_items':
                 $controller->productItems();
                 break;
+            case 'stock_purchase':
+                $controller->stockPurchase();
+                break;
             default:
                 $controller->index();
                 break;
