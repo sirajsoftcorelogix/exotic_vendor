885e3c830e8ceab5d4d735fa14e731b84b4c6408
hedayat-dev <hanwar.84@gmail.com>
2025-12-18 23:11:06 +0530

    update only more than 1

--- controllers/OrdersController.php
+++ controllers/OrdersController.php
@@ -911,6 +911,10 @@ class OrdersController {
                 // Map API fields to your table columns
                     
                 foreach ($order['cart'] as $item) {
+                    //check status other than 1 (pending)
+                    if(empty($item['order_status']) || $item['order_status'] == 1){
+                        continue;
+                    }
                     $rdata = [
                     'sku' => $item['sku'] ?? '',
                     'order_number' => $order['orderid'] ?? '',
