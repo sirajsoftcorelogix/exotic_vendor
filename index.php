3e6bf796baef6adbba023a01b1f651f670ca42fd
hedayat-dev <hanwar.84@gmail.com>
2025-12-18 15:34:18 +0530

    order update bulk

--- index.php
+++ index.php
@@ -145,6 +145,9 @@ switch ($page) {
             case 'update_import':
                 $controller->skuUpdateImportedOrders();
                 break;
+            case 'update_import_bulk':
+                $controller->ordersStatusImportBulk();
+                break;
             default:
                 $controller->index();
                 break;
