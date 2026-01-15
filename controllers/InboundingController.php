fc13a08c3f57b27425e5712623d4a3a36d2857d4
devhits <makwanavinod9211@gmail.com>
2026-01-15 22:56:20 +0530

    post_red

--- controllers/InboundingController.php
+++ controllers/InboundingController.php
@@ -1437,7 +1437,7 @@ class InboundingController {
             CURLOPT_FOLLOWLOCATION => true, // Follow any "Location: " headers (redirects)
             CURLOPT_MAXREDIRS => 10,        // Stop after 10 redirects to prevent infinite loops
             // ------------------
-            
+            CURLOPT_POSTREDIR => 3,
             CURLOPT_HTTPHEADER => $headers,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_SSL_VERIFYPEER => false, // Disable if SSL issue occurs
