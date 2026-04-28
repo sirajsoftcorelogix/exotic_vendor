628d570d95df3cd579758ef2252219e72867e0a8
Siraj Ali <siraj.php@gmail.com>
2026-04-28 20:36:41 +0530

    POS

--- views/pos_register/index.php
+++ views/pos_register/index.php
@@ -594,7 +594,7 @@
           <?php endif; ?>
           <!-- TOTALS -->
           <?php
-          print_array($cartData);
+          //print_array($cartData);
           $rawSubtotal = (float)($cartData['subtotal'] ?? 0);
           $gstTotal = (float)($cartData['gst'] ?? 0);
           $couponDiscount = (float)($cartData['coupon_discount'] ?? 0);
