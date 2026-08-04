fae32e04bea4bb602538761775c813694e00498d
Siraj Ali <siraj.php@gmail.com>
2026-08-04 10:52:22 +0530

    Revert "small fixing"
    This reverts commit 053d8480a822aa85b986cfaa96af684c2edc0abf.

--- models/invoice/DomesticEwbIrnService.php
+++ models/invoice/DomesticEwbIrnService.php
@@ -276,9 +276,8 @@ class DomesticEwbIrnService {
             ];
         }
         
-        // $invoiceNumberParts = explode('-', $invoice['invoice_number'] ?? '');
-        // $invoiceNumber = end($invoiceNumberParts);
-        $invoiceNumber = $invoice['invoice_number'] ?? '';
+        $invoiceNumberParts = explode('-', $invoice['invoice_number'] ?? '');
+        $invoiceNumber = end($invoiceNumberParts);
         $buyerAddress = ($customer['address_line1'] ?? '') . ' ' . ($customer['address_line2'] ?? '');
         $shippingAddress = ($customer['shipping_address_line1'] ?? '') . ' ' . ($customer['shipping_address_line2'] ?? '');
         $buyerDisplayName = trim((string)($customer['first_name'] ?? ''));
