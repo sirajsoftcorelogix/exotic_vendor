9343cccb4ed52fc9db9dbdd1da92ef556075c37c
Siraj Ali <siraj.php@gmail.com>
2026-08-31 20:15:07 +0530

    inbound 502 error

--- helpers/inbound_filter_dropdown_cache.php
+++ helpers/inbound_filter_dropdown_cache.php
@@ -106,7 +106,14 @@ function inbound_filter_dropdowns_get(Inbounding $model): array
 {
     $cached = inbound_filter_dropdowns_from_cookie();
     if ($cached !== null) {
+        if (isset($_GET['debug_step']) && $_GET['debug_step'] === '5_cache') {
+            die('DEBUG STEP 5_cache: Loaded filter dropdowns from Cookie cache!');
+        }
         return $cached;
+    }
+
+    if (isset($_GET['debug_step']) && $_GET['debug_step'] === '5_nocache') {
+        die('DEBUG STEP 5_nocache: Cookie cache MISS, proceeding to run SQL queries in getFilterDropdowns()');
     }
 
     $data = $model->getFilterDropdowns();
