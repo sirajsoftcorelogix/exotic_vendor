6ba676e4347699a4bafa244ab4d7a338a4726c6f
hedayat-dev <hanwar.84@gmail.com>
2026-09-11 00:53:05 +0530

    again

--- models/invoice/AlankitIrnNew.php
+++ models/invoice/AlankitIrnNew.php
@@ -458,7 +458,7 @@ return base64_encode($encryptedData);
             "EwbDtls" => 
                !empty($invoice['trans_id']) ? [
                 'TransId' => (string)($invoice['trans_id'] ?? ''),
-                'TransName' => (string)($invoice['trans_name'] ?? ''),  
+                'TransName' => (string)($invoice['trans_name'] ?? ''),
                 'Distance' => 0
                 ] : [
                 'Distance' => 0,
